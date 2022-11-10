b0a1d811b130175a363b6e2a312e819db780e647
KickButtowski80 <pazpaz22@yahoo.com>
2022-11-09 18:43:32 -0800

    document get all wrokouts point by using swagger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const dotenv = require('dotenv');
+dotenv.config();
+
 const router = express.Router();
 
 const apicache = require('apicache')
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
-
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 const app = express();
 
 const cache = apicache.middleware
@@ -15,8 +19,13 @@ app.use(bodyParser.json());
 app.use(cache('2 minutes'))
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
-
+// app.use( (request, response, next) => {
+//   response.header("Access-Control-Allow-Origin", "*");
+//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
+  V1SwaggerDocs(app, PORT);
 });
